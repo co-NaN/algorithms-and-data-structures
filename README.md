a38fa14abbf162c6c3b56a75cc4aeed590c92d97
polya.daniel <polya.daniel@dss.hu>
2019-05-01 20:58:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Algorithms and data structures
+
+Solutions to algorithmic problems, written in JavaScript. Partially based on https://github.com/StephenGrider/AlgoCasts, especially the tests.
---
+++ reversestring/index.js
@@ -0,0 +1,48 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+    return str === '' ? '' : reverse(str.substring(1)) + str.charAt(0);
+}
+
+function reverse2(str) {
+    let reversed = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
+}
+
+function reverse3(str) {
+    return str
+        .split('')
+        .reverse()
+        .join('');
+}
+
+function reverse4(str) {
+    let reversed = '';
+    for (let character of str) {
+        reversed = character + reversed;
+    }
+    return reversed;
+}
+
+function reverse5(str) {
+    return str.split('').reduce((reverse, char) => char + reverse, '');
+}
+
+function reverse6(str) {
+    let reverse = '';
+    for (let i = 0; i < str.length; i++) {
+        reverse = str[i] + reverse;
+    }
+    return reverse;
+}
+
+module.exports = reverse;
