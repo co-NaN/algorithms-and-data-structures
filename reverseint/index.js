4b53d64e92f7f8f2cec56771ae21a32be0a1383d
co-NaN <dgyarmati@protonmail.com>
2019-05-05 18:19:56 +0200

    Handling zero.

--- reverseint/index.js
+++ reverseint/index.js
@@ -8,6 +8,18 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
+function reverseInt(num) {
+    let digits = splitToDigits(num);
+    let reversed = num;
+    if (num !== 0) {
+        reversed = digits[digits.length - 1];
+        for (let i = digits.length - 1; i > 0; i--) {
+            reversed = (reversed * 10) + digits[i - 1];
+        }
+    }
+    return Math.sign(num) * reversed;
+}
+
 function splitToDigits(num) {
     num = Math.abs(num);
     let digits = [];
@@ -19,16 +31,29 @@ function splitToDigits(num) {
     return digits;
 }
 
-function reverseInt(num) {
-    let digits = splitToDigits(num);
-    let reversed = digits[digits.length - 1];
-    for (let i = digits.length - 1; i > 0; i--) {
-        reversed = (reversed * 10) + digits[i - 1];
-    }
-    if (num < 0) {
-        reversed = -1 * reversed;
-    }
-    return reversed;
-}
+// naive solution, using string conversion
+// function reverseInt(n) {
+//     let negative = false;
+//     if (Math.sign(n) === -1) {
+//         n = n * -1;
+//         negative = true;
+//     }
+//     let digits = n.toString().split('');
+//     let reversed = '';
+//     for (let digit of digits) {
+//         reversed = digit + reversed;
+//     }
+//     return negative ? -1 * parseInt(reversed) : parseInt(reversed);
+// }
+
+// naive solution no. 2, somewhat more concise
+// function reverseInt(n) {
+//     const reversed = n
+//         .toString()
+//         .split('')
+//         .reverse()
+//         .join('');
+//     return Math.sign(n) * parseInt(reversed);
+// }
 
 module.exports = reverseInt;
