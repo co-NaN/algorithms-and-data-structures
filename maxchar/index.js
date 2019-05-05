cd7b9a283a17d2235fbb82b02d16602f1903b47d
co-NaN <dgyarmati@protonmail.com>
2019-05-05 19:13:03 +0200

    Solution draft: finding highest frequency char in string.

---
+++ maxchar/index.js
@@ -0,0 +1,27 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+// rough draft of first solution
+function maxChar(str) {
+    let frequencies = {};
+    let max = 1;
+    let maxChar = str.charAt(0);
+    for (let char of str) {
+        if (frequencies.hasOwnProperty(char)) {
+            frequencies[char]++;
+            if (frequencies[char] > max) {
+                max = frequencies[char];
+                maxChar = char;
+            }
+        } else {
+            frequencies[char] = 1;
+        }
+    }
+    return maxChar;
+}
+
+module.exports = maxChar;
