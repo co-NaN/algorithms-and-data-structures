65ad4e6861b0529d82a438555ed27811e1327f3e
co-NaN <dgyarmati@protonmail.com>
2019-05-19 12:05:49 +0200

    Cleanup.

--- vowels/solutionRegex.js
+++ vowels/solutionRegex.js
@@ -1,4 +1,4 @@
 function vowels(str) {
-    const vowels = str.match(/[aeiouAEIOU]/g);
-    return vowels === null ? 0 : vowels.length;
+    const vowels = str.match(/[aeiou]/gi);
+    return vowels ? vowels.length : 0;
 }
